7564f032f02c542762b9618b6171baafd27dc7b9
Antón_Rodríguez_Seoane <144905749+AntonRSeoane@users.noreply.github.com>
2024-02-05 13:00:34 +0100

    Add files via upload

---
+++ ConexionFicheros/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConexionFicheros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ruta = Directory.GetCurrentDirectory();
+            string rutaFichero = ruta + @"\series.txt";
+            Console.WriteLine("Ruta actual: " + rutaFichero);
+
+            StreamWriter escritor = new StreamWriter(rutaFichero, false);
+
+            escritor.WriteLine("Death Note*Crunchyroll");
+
+            escritor.Close();
+
+            StreamReader lector = new StreamReader(rutaFichero);
+
+
+            Console.ReadLine();
+        }
+    }
+}
